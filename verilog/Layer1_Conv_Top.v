396a9a115e0331b769c84ad195fbc2dbcdbfc1f8
ztl771983350 <50801547+ztl771983350@users.noreply.github.com>
2021-03-27 13:58:03 +0800

    Update Layer1_Conv_Top.v
    更新排版

--- verilog/Layer1_Conv_Top.v
+++ verilog/Layer1_Conv_Top.v
@@ -19,45 +19,45 @@
 //
 //////////////////////////////////////////////////////////////////////////////////
 module Layer1_Conv_Top#(
-	parameter									bits						=	16		,	//quantization bit number
-	parameter									bits_shift				=	4		,	//we can shift but not multy
-	parameter									channel_num				=	4		,
-	parameter									channel_height_num	=	4		,
-	parameter									channel_length_num	=	4		,
-	parameter									channel_paralell_num	=	16		,
-	parameter									channel_in_num 		=	16		,
-	parameter									channel_in_num_2		=	5		,
-	parameter									channel_out_num		=	64		,
-	parameter									pool_stride_length	=	2		,
-	parameter									pool_stride_height  	=	2		,
-	parameter									pool_stride_num_length = 2		,
-	parameter									pool_stride_num_height = 2		,
-	parameter									pool_channel_heignt	=	2		,
-	parameter									pool_channel_length	=	2		,
-	parameter									pool_channel			=	4		,
-	parameter									pool_channel_bits		=	256		,
-	parameter									pool_channel_bits_shift	=	8	,
-	parameter 									conv_num					=	64		,	//channel_num*channel_in_num
-	parameter									filter_size				=	25		,
-	parameter									filter_size_2			=	5		
+	parameter					bits			=	16		,	//quantization bit number
+	parameter					bits_shift		=	4		,	//we can shift but not multy
+	parameter					channel_num		=	4		,
+	parameter					channel_height_num	=	4		,
+	parameter					channel_length_num	=	4		,
+	parameter					channel_paralell_num	=	16		,
+	parameter					channel_in_num 		=	16		,
+	parameter				        channel_in_num_2	=	5		,
+	parameter					channel_out_num		=	64		,
+	parameter					pool_stride_length	=	2		,
+	parameter					pool_stride_height  	=	2		,
+	parameter					pool_stride_num_length  =       2		,
+	parameter					pool_stride_num_height  =       2		,
+	parameter					pool_channel_heignt	=	2		,
+	parameter				        pool_channel_length	=	2		,
+	parameter					pool_channel		=	4		,
+	parameter					pool_channel_bits	=	256		,
+	parameter					pool_channel_bits_shift	=	8	,
+	parameter 					conv_num		=	64		,	//channel_num*channel_in_num
+	parameter					filter_size		=	25		,
+	parameter					filter_size_2		=	5		
 	)
 	(
-	input																clk_in,
-	input 															rst_n,
+	input								clk_in,
+	input 								rst_n,
 	input 	[(channel_in_num<<bits_shift)-1:0]			data_in,
-	input																start,
-	input		[(channel_num<<bits_shift)-1:0]				weights,
-	output	reg [(channel_out_num<<bits_shift)-1:0]	data_out,
-	output															ready
+	input								start,
+	input		[(channel_num<<bits_shift)-1:0]			weights,
+	output	reg [(channel_out_num<<bits_shift)-1:0]   		data_out,
+	output								ready
     );
 
 wire				[bits-1:0]				data_in_conv			[0:channel_in_num-1]		; 
 wire				[bits-1:0]				weight_in_conv			[0:channel_num-1]			;
-wire	signed	[(bits<<1)-1+filter_size_2:0]					data_conv					[0:conv_num-1]				;
-wire											ready_conv				[0:conv_num-1]				;
-assign 										ready			=			ready_conv[0]				;
-wire	signed	[(bits<<1)-1:0]		bias						[0:channel_num-1]			;
-wire				[pool_channel_bits-1:0]data_temp				[0:pool_channel-1]		;
+wire	signed	[(bits<<1)-1+filter_size_2:0]				data_conv				[0:conv_num-1]				;
+wire									ready_conv				[0:conv_num-1]				;
+assign 									ready			=		ready_conv[0]				;
+wire	signed	[(bits<<1)-1:0]		bias				[0:channel_num-1]			;
+wire				[pool_channel_bits-1:0]data_temp	[0:pool_channel-1]		;
 assign										bias[3]		=			32'b00100010001011000011010000000000	;																				 
 assign										bias[2]		=			32'b00000110100101101100110010111000	;
 assign										bias[1]		=			32'b00001001111010011000011110110000	;
